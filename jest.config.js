d2a3a9b08d48de1443f5608373fbf89f8cfafb16
Vegard Haugstvedt <it.vegard@gmail.com>
2023-05-23 10:34:15 +0200

    Init Jest and Testing Library setup (using SWC instead of Babel/ts-jest)

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    transform: {
+        "^.+\\.(t|j)sx?$": "@swc/jest",
+    },
+};
