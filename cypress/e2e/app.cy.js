be5547e4ba6a31670465fdbd1564e33638651c5c
Vegard Haugstvedt <it.vegard@gmail.com>
2023-05-25 15:46:54 +0200

    Add cypress tests with axe

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,27 @@
 describe("Cypress tests", () => {
     it("should load the app", () => {
         cy.visit("http://localhost:3000/");
+        cy.injectAxe();
+        cy.configureAxe({
+            rules: [
+                { id: "svg-img-alt", enabled: false },
+                { id: "image-alt", enabled: false }
+            ]
+        })
+        cy.checkA11y(null, {
+            rules: {
+                'svg-img-alt': { enabled: false }
+            }
+        });
+
+        cy.contains("Start her").click();
+
+        cy.url().should('include', '/oppgaver/1-linting')
+        
+        cy.checkA11y(null, {
+            rules: {
+                'image-alt': { enabled: false }
+            }
+        });
     })
 })
