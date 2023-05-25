a60ff746e5cf879cfb47e93de08d8ee811a67037
Vegard Haugstvedt <it.vegard@gmail.com>
2023-05-25 14:03:28 +0200

    Med axe-tester

--- src/app/oppgaver/2-enhetstesting/TestComponent.tsx
+++ src/app/oppgaver/2-enhetstesting/TestComponent.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function TestComponent() {
     return (
-        <button aria-pressed="false">Toggle button</button>
+        <button aria-pressed="bullshit">Toggle button</button>
     )
 }
--- src/app/oppgaver/2-enhetstesting/page.test.tsx
+++ src/app/oppgaver/2-enhetstesting/page.test.tsx
@@ -6,9 +6,13 @@ import {render, screen} from '@testing-library/react'
 import TestComponent from "./TestComponent"
 import { axe, toHaveNoViolations } from "jest-axe";
 
+expect.extend(toHaveNoViolations);
+
 describe("My first test", () => {
     it("checks if the TestComponent is accessible", async () => {
-        render(<TestComponent />);
+        const { container } = render(<TestComponent />);
         await screen.findByRole('button');
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
     })
 })
