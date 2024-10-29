6a577cb91d6a014fa6c308b886b47da13f29ca74
Vegard Haugstvedt <it.vegard@gmail.com>
2024-10-29 03:55:33 +0100

    Update unit test file with tasks and make tests fail until edited

--- src/app/oppgaver/2-enhetstesting/tests/toggle-button/ToggleButton.test.tsx
+++ src/app/oppgaver/2-enhetstesting/tests/toggle-button/ToggleButton.test.tsx
@@ -9,13 +9,22 @@ import { axe, toHaveNoViolations } from "jest-axe";
 expect.extend(toHaveNoViolations);
 
 describe("ToggleButton", () => {
+  /* Task 1
+   *
+   * Use the findByRole function to look up the ToggleButton component by the role, and check that it exists.
+   */
   it("renders correctly", async () => {
     const { findByRole } = render(<ToggleButton text="Toggle Button" />);
 
-    /* Task 1 */
+    throw new Error("Task 1 not implemented");
   });
 
-  it("works atomically, allowing more than one to be used simultaneously", async () => {
+  /* Task 2
+   *
+   * Check that the second ToggleButton exists.
+   * Hint: Use the accessible name to separate the two buttons.
+   */
+  it("has accessible name matching the visible text", async () => {
     const { findByRole } = render(
       <>
         <ToggleButton text="Toggle 1" />
@@ -23,9 +32,15 @@ describe("ToggleButton", () => {
       </>
     );
 
-    /* Task 2 */
+    throw new Error("Task 2 not implemented");
   });
 
+  /* Task 3
+   *
+   * Check that both ToggleButtons exist and are unpressed.
+   * Click "Toggle 1" and check that it gets the attribute "aria-pressed" set to "true" (string).
+   * Check that "Toggle 2" still has "aria-pressed" set as "false".
+   */
   it("correctly sets [pressed] state", async () => {
     const { findByRole } = render(
       <>
@@ -34,17 +49,6 @@ describe("ToggleButton", () => {
       </>
     );
 
-    /* Task 3 */
-  });
-
-  it("is accessible", async () => {
-    const { container, findByRole } = render(
-      <>
-        <ToggleButton text="Toggle 1" />
-        <ToggleButton text="Toggle 2" />
-      </>
-    );
-
-    /* Task 4 */
+    throw new Error("Task 3 not implemented");
   });
 });
