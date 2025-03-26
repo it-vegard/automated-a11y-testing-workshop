afa08065c9bc4cf7c473288a25019826e4753237
Vegard Haugstvedt <it.vegard@gmail.com>
2025-03-26 13:26:37 +0100

    Ignore playwright tests when running jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/tests/",
+  ],
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
