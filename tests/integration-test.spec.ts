20ba84382a9a104235780d9d368e949322a4d415
Vegard Haugstvedt <it.vegard@gmail.com>
2025-03-26 13:35:12 +0100

    Update and prepare integration test file

--- tests/integration-test.spec.ts
+++ tests/integration-test.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
-test("has title", async ({ page }) => {
+test("test webpage", async ({ page }) => {
   await page.goto("http://localhost:3000/oppgaver/3-ende-til-ende-testing");
 });
